11bc97f4a8e03aaea65fee1add08dc0855b26813
Wishforpeace <1903180340@qq.com>
2023-01-16 16:52:02 +0800

    feat:zrpc

--- service/search/api/internal/logic/searchlogic.go
+++ service/search/api/internal/logic/searchlogic.go
@@ -2,7 +2,9 @@ package logic
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"go-zero-demo/service/user/rpc/userclient"
 
 	"go-zero-demo/service/search/api/internal/svc"
 	"go-zero-demo/service/search/api/internal/types"
@@ -25,8 +27,21 @@ func NewSearchLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchLogi
 }
 
 func (l *SearchLogic) Search(req *types.SearchReq) (resp *types.SearchReply, err error) {
-	// todo: add your logic here and delete this line
-	logx.Infof("userId: %v", l.ctx.Value("userId")) // 这里的key和生成jwt token时传入的key一致
-	fmt.Println(l.ctx.Value("userId"))
-	return &types.SearchReply{}, nil
+	userIdNumber := json.Number(fmt.Sprintf("%v", l.ctx.Value("userId")))
+	logx.Infof("userId: %s", userIdNumber)
+	userId, err := userIdNumber.Int64()
+	if err != nil {
+		return nil, err
+	}
+	// 使用user rpc
+	_, err = l.svcCtx.UserRpc.GetUser(l.ctx, &userclient.IdReq{
+		Id: userId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &types.SearchReply{
+		Name:  req.Name,
+		Count: 100,
+	}, nil
 }
--- service/search/api/internal/svc/servicecontext.go
+++ service/search/api/internal/svc/servicecontext.go
@@ -2,18 +2,22 @@ package svc
 
 import (
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 	"go-zero-demo/service/search/api/internal/config"
 	"go-zero-demo/service/search/api/internal/middleware"
+	"go-zero-demo/service/user/rpc/userclient"
 )
 
 type ServiceContext struct {
 	Config  config.Config
 	Example rest.Middleware
+	UserRpc userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:  c,
 		Example: middleware.NewExampleMiddleware().Handle,
+		UserRpc: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
